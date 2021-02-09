1b48b3219eeac6ebb353caacc923b123f49ac6db
Antoine Nattier <antoinenattier@macbook-pro-de-antoine.home>
2021-02-09 20:19:31 +0100

    Task02

--- Task02/my_putstr.c
+++ Task02/my_putstr.c
@@ -3,10 +3,9 @@ int my_putstr(char const *str)
   int i; // je créé une variable i
   i = 0; // je la met à 0 
 
-  while (str[i] != '\0') // je lui dis d'etre dans une boucle en mode je peux mettre autant des index que je souhaite
-    {
+  while (str[i] != '\0'){ // je lui dis d'etre dans une boucle en mode je peux mettre autant des index que je souhaite
       my_putchar(str[i]); // j'affiche mon résultat
-      i++; // je lui fait prendre un + 1 à chaque fois 
+      i = i + 1; // je lui fait prendre un + 1 à chaque fois 
     }
   return (0);
 }
