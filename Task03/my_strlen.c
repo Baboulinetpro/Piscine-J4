71669c76b6a18764d8965a980d11e129f9d95927
Antoine Nattier <antoinenattier@macbook-pro-de-antoine.home>
2021-02-09 21:09:40 +0100

    Task03

--- Task03/my_strlen.c
+++ Task03/my_strlen.c
@@ -1,11 +1,10 @@
 int my_strlen(const char* str)
 {
-  int i; // je créé une variable i
-  i = 0; // je la met à 0 
+  int i; /* je créé une variable i */
+  i = 0; /* je la met à 0 */ 
 
-  while (str[i] != '\0') // je lui dis d'etre dans une boucle en mode je peux mettre autant des index que je souhaite
-    {
-      i++; // je lui fait prendre un + 1 à chaque fois 
+  while (str[i] != '\0') {/* je lui dis d'etre dans une boucle en mode je peux mettre autant des index que je souhaite */
+    i = i + 1; /* je lui fait prendre un + 1 à chaque fois */ 
     }
-  return (i); // retourne le nombre de i
+  return (i); /* retourne le nombre de i */
   }
